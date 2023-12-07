17407e304849185e5d22f9a57238a0d19fcf70ab
Colum Kelly <columk1@users.noreply.github.com>
2023-12-07 11:40:21 -0800

    Day 7

---
+++ 2023/07/solution.js
@@ -0,0 +1,108 @@
+const fs = require('fs')
+let jack = 'C'
+let input = fs
+  .readFileSync('input.txt')
+  .toString()
+  .replaceAll('T', 'B')
+  .replaceAll('J', jack)
+  .replaceAll('Q', 'D')
+  .replaceAll('A', 'Z')
+  .split('\n')
+  .map((x) => x.split(' '))
+
+function getStrength(hand) {
+  let cards = hand.split('')
+  if (cards.every((e) => e === cards[0])) return 7
+  if (hand.match(/([a-zA-Z0-9]).*\1.*\1.*\1/)) return 6
+  let threePair = hand.match(/([a-zA-Z0-9]).*\1.*\1/)
+  if (threePair) {
+    return cards.filter((e) => e !== threePair[1]).every((e, i, a) => e === a[0]) ? 5 : 4
+  } else {
+    let pair = hand.match(/([a-zA-Z0-9]).*\1/)
+    if (pair) {
+      return cards
+        .filter((e) => e !== pair[1])
+        .toString()
+        .match(/([a-zA-Z0-9]).*\1/)
+        ? 3
+        : 2
+    }
+    return 1
+  }
+}
+
+function part1() {
+  input.forEach((player) => {
+    player.push(getStrength(player[0]))
+  })
+  input.sort((a, b) => {
+    if (a[2] > b[2]) return 1
+    if (a[2] < b[2]) return -1
+    if (a[2] === b[2]) {
+      for (let i = 0; i < a[0].length; i++) {
+        if (a[0][i] > b[0][i]) return 1
+        if (a[0][i] < b[0][i]) return -1
+      }
+    }
+  })
+  return input.reduce((acc, curr, i) => acc + curr[1] * (i + 1), 0)
+}
+
+console.log(part1())
+
+// Part 2
+
+function getMode(arr) {
+  let copy = [...arr]
+  return copy
+    .filter((e) => e !== jack)
+    .sort((a, b) => copy.filter((v) => v === a).length - copy.filter((v) => v === b).length)
+    .pop()
+}
+
+function getStrengthB(hand) {
+  // console.log({hand})
+  let cards = hand.split('')
+  let mode = getMode(cards)
+  cards = cards.map((e) => (e === jack ? mode : e))
+  hand = hand.replaceAll(jack, mode)
+  // console.log({cards})
+  // console.log({hand})
+  if (cards.every((e) => e === cards[0])) return 7
+  if (hand.match(/([a-zA-Z0-9]).*\1.*\1.*\1/)) return 6
+  let threePair = hand.match(/([a-zA-Z0-9]).*\1.*\1/)
+  if (threePair) {
+    return cards.filter((e) => e !== threePair[1]).every((e, i, a) => e === a[0]) ? 5 : 4
+  } else {
+    let pair = hand.match(/([a-zA-Z0-9]).*\1/)
+    if (pair) {
+      return cards
+        .filter((e) => e !== pair[1])
+        .toString()
+        .match(/([a-zA-Z0-9]).*\1/)
+        ? 3
+        : 2
+    }
+    return 1
+  }
+}
+
+function part2() {
+  input.forEach((player) => {
+    player.push(getStrengthB(player[0]))
+    player[0] = player[0].replaceAll(jack, '1')
+  })
+  input.sort((a, b) => {
+    if (a[2] > b[2]) return 1
+    if (a[2] < b[2]) return -1
+    if (a[2] === b[2]) {
+      for (let i = 0; i < a[0].length; i++) {
+        if (a[0][i] > b[0][i]) return 1
+        if (a[0][i] < b[0][i]) return -1
+      }
+    }
+  })
+  return input.reduce((acc, curr, i) => acc + curr[1] * (i + 1), 0)
+}
+
+console.log(part2())
